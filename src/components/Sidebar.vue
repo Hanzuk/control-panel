a31cd1d5861fb849d1c8333b4162cab23f8fd5bd
Roberto Duarte <robert0d3@outlook.com>
2019-11-04 15:13:41 -0600

    Sidebar and Menu components unified

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,7 +7,43 @@
 			<img src="@/assets/logo.svg" alt="Logo" />
 		</div>
 		<div class="sidebar-body p-4 overflow-y-auto">
-			<Menu />
+			<nav class="relative">
+				<span
+					class="absolute w-full h-10 rounded transition-transform transition-500 transition-backward bg-amethyst-300"
+					:style="{ transform: `translateY(${100 * selected}%)` }"
+				></span>
+				<ul class="relative">
+					<li v-for="item in menuItems" :key="item">
+						<button
+							class="flex items-center w-full h-10 px-3 py-2 focus:outline-none"
+							@click="
+								select(menuItems.indexOf(item));
+								$router.push({ name: item.toLowerCase() });
+								$store.dispatch('ux/closeSidebar');
+							"
+						>
+							<Icon
+								:name="item.toLowerCase()"
+								class="w-6 h-6 fill-current transition-color transition-500 transition-backward"
+								:class="
+									selected === menuItems.indexOf(item)
+										? 'text-light-100'
+										: 'text-dark-500'
+								"
+							/>
+							<span
+								class="ml-2 text-sm font-normal transition-color transition-500 transition-backward"
+								:class="
+									selected === menuItems.indexOf(item)
+										? 'text-light-100'
+										: 'text-dark-500'
+								"
+								>{{ item }}</span
+							>
+						</button>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	</div>
 </template>
@@ -15,13 +51,19 @@
 <script>
 import { mapState } from 'vuex';
 
-import Menu from '@/components/Menu.vue';
-
 export default {
-	components: {
-		Menu
+	data() {
+		return {
+			menuItems: ['Dashboard', 'Reports', 'Security'],
+			selected: 0
+		};
 	},
-	computed: mapState(['ux'])
+	computed: mapState(['ux']),
+	methods: {
+		select(i) {
+			this.selected = i;
+		}
+	}
 };
 </script>
 
