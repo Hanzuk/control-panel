03a9875023a555af4c07fb8cb89965d88da3eadc
Roberto Duarte <robert0d3@outlook.com>
2019-11-04 15:14:25 -0600

    Column specific width only on large screen

--- src/views/Security.vue
+++ src/views/Security.vue
@@ -2,12 +2,12 @@
 <template>
 	<Columns>
 		<template>
-			<Column class="w-1/3 h-84">
+			<Column class="lg:w-1/3 h-84">
 				<h2 class="text-dark-700">
 					This is the Security view. Put your stuff here.
 				</h2>
 			</Column>
-			<Column class="w-4/6 h-84">
+			<Column class="lg:w-4/6 h-84">
 				<h2 class="text-dark-700">
 					This is the Security view. Put your stuff here.
 				</h2>
@@ -17,12 +17,12 @@
 					This is the Security view. Put your stuff here.
 				</h2>
 			</Column>
-			<Column class="w-3/4 h-84">
+			<Column class="lg:w-3/4 h-84">
 				<h2 class="text-dark-700">
 					This is the Security view. Put your stuff here.
 				</h2>
 			</Column>
-			<Column class="w-3/12 h-84">
+			<Column class="lg:w-3/12 h-84">
 				<h2 class="text-dark-700">
 					This is the Security view. Put your stuff here.
 				</h2>
